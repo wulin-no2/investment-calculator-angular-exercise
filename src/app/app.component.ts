451042e3fa2e7ffc55b559ce7bce8b75cb5f4460
Lina <wulinthu@gmail.com>
2026-01-15 12:59:10 +0000

    Adding form html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { CalculatorComponent } from "./calculator/calculator.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CalculatorComponent],
 })
 export class AppComponent {}
---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.css'
+})
+export class CalculatorComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+
+}
